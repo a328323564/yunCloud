4a95cac198cee9ec343f363ec84fd8256638e6cd
a328323564 <328323564@qq.com>
2018-12-13 09:40:07 +0800

    搭建了rest和manager小框架

---
+++ symphony-dubbo-server-api/src/main/java/cn/symphony/dubbo/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.symphony.dubbo.service;
+
+import cn.symphony.dubbo.pojo.User;
+
+public interface UserService {
+	
+	//用户登录
+	public User login(User user);
+	
+}
---
+++ symphony-manager/symphony-manager-service/src/main/java/org/symphony/manager/service/App.java
@@ -0,0 +1,13 @@
+package org.symphony.manager.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
