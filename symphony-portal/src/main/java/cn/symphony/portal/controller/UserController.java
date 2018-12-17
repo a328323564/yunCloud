5150694ef66f507fb60775bbfe89d01b4fd0aaae
a328323564 <328323564@qq.com>
2018-12-17 23:34:42 +0800

    ssm搭建完成

--- symphony-portal/src/main/java/cn/symphony/portal/controller/UserController.java
+++ symphony-portal/src/main/java/cn/symphony/portal/controller/UserController.java
@@ -1,9 +1,8 @@
 package cn.symphony.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,47 +18,7 @@ public class UserController {
 	//通过dubbo注入订阅服务
  	@Reference
  	private UserService userService;
-	
-	
-	/*@RequestMapping(value = "/login")
-	public void login(@RequestParam Map<String, Object> map, HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-		// 设置响应MIME和编码
-		resp.setContentType("application/json;charset=utf-8");
-		// 获取输出流
-		PrintWriter out = resp.getWriter();
-
-		// 创建返回结果对象
-		SymphonyResult symResult = null;
-		// 获取session中的验证码
-		String rand = (String) req.getSession().getAttribute("rand");
-
-		System.out.println("进来了");
-		String username = (String) map.get("uname");
-		String password = (String) map.get("pwd");
-		String verifyCode = (String) map.get("verifyCode");
-
-		System.out.println(username);
-		System.out.println(password);
-		System.out.println(verifyCode);
-
-		// 先校验验证码，通过后才开始校验密码和账号
-		if (!verifyCode.toLowerCase().equals(rand.toLowerCase())) {
-			System.out.println("验证码错误");
-			symResult = new SymphonyResult(0, "验证码错误", null);
-		} else {
-			//使用dubbo开发。创建rest项目
-			
-		}
-
-		String symJSON = JsonUtils.objectToJson(symResult);
-		// 将json对象响应到浏览器
-		out.print(symJSON);
-		out.flush();// 强制刷新
-		out.close();
-	}
-	
-	*/
+ 	
 	@RequestMapping("/test")
 	@ResponseBody
 	public String test() {
@@ -72,6 +31,19 @@ public class UserController {
 		System.out.println("来自供应商的信息：" + login.toString());
 		return "这是dubbo中文.";
 	}
+	
+	
+	@RequestMapping("/test2")
+	@ResponseBody
+	public String test3() {
+		
+		
+		
+		return "这是dubbo中文.";
+	}
+	
+	
+	
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String test2() {
--- symphony-rest/src/main/java/cn/symphony/rest/service/impl/UserServiceImpl.java
+++ symphony-rest/src/main/java/cn/symphony/rest/service/impl/UserServiceImpl.java
@@ -1,20 +1,38 @@
 package cn.symphony.rest.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.alibaba.dubbo.config.annotation.Service;
 import cn.symphony.dubbo.api.pojo.User;
 import cn.symphony.dubbo.api.service.UserService;
+import cn.symphony.manager.mapper.SymUserMapper;
+import cn.symphony.manager.pojo.SymUser;
+import cn.symphony.manager.pojo.SymUserExample;
+import cn.symphony.manager.pojo.SymUserExample.Criteria;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	private SymUserMapper mapper;
+	
 	@Override
 	public User login(User user) {
+		System.out.println("来到这里了");
 		System.out.println("来到rest供应商");
 		System.out.println("消费者传过来的User是：" + user.toString());
 		User user1 = new User();
 		user1.setUsername("xiaoming消费者");
 		user1.setPassword("123");
+		
+		
+		SymUser res = mapper.selectByPrimaryKey(1);
+		System.out.println(res.toString());
+		
 		return user1;
+		
 	}
 	
 }
